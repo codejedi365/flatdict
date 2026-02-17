67c423971790c4bcae7ef8fb7aa4b58d33a8868b
codejedi365 <codejedi365@gmail.com>
2026-02-17 00:46:02 -0700

    chore: add BSD-3-Clause copyright notice to module base file

--- src/cj365/flatdict/__init__.py
+++ src/cj365/flatdict/__init__.py
@@ -1,3 +1,17 @@
+"""
+This module provides the `FlatDict` and `FlatterDict` classes for
+managing nested dictionaries with flattened keys. The `FlatDict` class
+allows for simple key-value storage with support for nested structures
+using a specified delimiter. The `FlatterDict` class extends this
+functionality to handle lists and sets as child-dict instances with
+the offset as the key.
+"""
+# +-------------------------------------------------------------------+
+# | Module: cj365.flatdict                                            |
+# | Author: codejedi365, 2026                                         |
+# | Licensed under the BSD-3-Clause License.                          |
+# +-------------------------------------------------------------------+
+
 from importlib.metadata import version as get_metadata_version
 
 from cj365.flatdict.flat_dict import FlatDict
